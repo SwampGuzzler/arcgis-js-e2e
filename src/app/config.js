be66a72280c2b68c9d67f9ef53cbae632ac977e9
Lucas Cotner <lcotner@blueraster.com>
2015-09-15 10:33:02 -0400

    Initialized project using odoenet's awesome yeoman generator, adding tests and configurations

---
+++ src/app/config.js
@@ -0,0 +1,14 @@
+var config = {
+  /*
+  webmap: {
+    webmapid: 'addawebmapidhere'
+  },
+  */
+  mapOptions: {
+    center: [-122.3, 47.62],
+    zoom: 12,
+    basemap: 'topo'
+  }
+};
+
+export default config;
